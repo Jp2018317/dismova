a3de2f5d4e15796e04d7497e46ac45b8ede48d2a
jmorales <jmorales-2018317@kinal.edu.gt>
2023-09-15 01:27:22 -0600

    auth design improved

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -2,15 +2,25 @@ import { montserrat } from '@/lib/fonts';
 import SignupForm from '@/components/signup/signup-form';
 import Link from 'next/link';
 import { ROUTES } from '@/config';
+import { BiChevronLeft } from 'react-icons/bi';
+import Logo from '@/components/logo';
 
 export default async function SignUpPage() {
   return (
-    <section className="w-full grid place-items-center">
-      <div className="w-1/2 h-1/2 space-y-9">
-        <h1 className={`text-center text-6xl ${montserrat.className}`}>DISMOVA</h1>
-        <h2 className={`text-center text-2xl ${montserrat.className}`}>Registro</h2>
+    <section className="relative w-full grid place-items-center">
+      <button type="button" className="absolute top-5 left-5 flex items-center text-sm">
+        <BiChevronLeft className="w-8 h-8 text-zinc-500" />
+      </button>
+      <div className="w-full max-w-lg px-10 space-y-4">
+        <div
+          className="lg:hidden flex flex-col items-center justify-center"
+        >
+          <Logo className="xs:w-60 xs:h-24 w-48 h-20" />
+          <p className={`text-2xl tracking-[1rem] pl-[1rem] font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#FF512F] to-[#DD2476] ${montserrat.className}`}>DISMOVA</p>
+        </div>
+        <h2 className={`text-center text-xl lg:text-3xl ${montserrat.className}`}>REGISTRO</h2>
         <SignupForm />
-        <Link href={ROUTES.auth.login} className="underline mt-1.5 text-xs">Ya tienes cuenta?</Link>
+        <Link href={ROUTES.auth.login} className="underline mt-2 text-xs">Ya tienes cuenta?</Link>
       </div>
     </section>
   );
