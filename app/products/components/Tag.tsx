5c2a6b808158979b5dd6275b6986aefb10f2daff
jmorales <jmorales-2018317@kinal.edu.gt>
2023-09-24 22:20:32 -0600

    Design improve ( drawer bug )

--- app/products/components/Tag.tsx
+++ app/products/components/Tag.tsx
@@ -8,7 +8,7 @@ type LabelProps = {
 export default function Tag({ icon, title }:LabelProps) {
   return (
     <li>
-      <div className="h-14 lg:h-20 max-w-sm bg-zinc-300 dark:bg-zinc-800 rounded-lg p-1 flex justify-center items-center">
+      <div className="h-14 lg:h-20 max-w-sm bg-secondary rounded-lg p-1 flex justify-center items-center">
         {icon}
       </div>
       <p className="text-center dark:text-zinc-300 mt-2 text-[10px] lg:text-sm font-semibold">{ title }</p>
