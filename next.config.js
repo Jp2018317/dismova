625a5e266997b2031c97f46247bd489805277cf0
jmorales <jmorales-2018317@kinal.edu.gt>
2023-11-04 18:54:39 -0600

    supabase storage domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['ttcctffsichnykxnkaob.supabase.co'],
+    },
+}
 
 module.exports = nextConfig
