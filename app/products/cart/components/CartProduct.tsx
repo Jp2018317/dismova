6a7d8dd4e8e498501bb2fc1e051fd741e50b1ed7
jmorales <jmorales-2018317@kinal.edu.gt>
2023-11-15 23:24:52 -0600

    Add products to shopping cart

--- app/products/cart/components/CartProduct.tsx
+++ app/products/cart/components/CartProduct.tsx
@@ -23,6 +23,7 @@ export default function CartProduct({
   shortTitle, description, price, stock, category, code,
 }: CartItem) {
   const [image, setImage] = useState(`/images/${category}/${shortTitle}.webp`);
+
   return (
     <div className="w-full border border-border rounded-xl flex h-36 sm:h-44">
       <div className="bg-secondary rounded-l-xl h-full flex items-center">
